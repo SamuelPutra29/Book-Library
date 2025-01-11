29efa98a2bd87f4e4a4d6f12c8da4796f2ef46db
SAMUELPUTRA\samue <samueldianpermanaputra@gmail.com>
2025-01-11 15:10:55 +0100

    Initial commit

--- book-api/book-api/src/main/java/com/example/book_api/repository/BookRepository.java
+++
@@ -1,5 +0,0 @@
-package com.example.book_api.repository;
-import com.example.book_api.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {}
